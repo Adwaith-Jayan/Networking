7888c7e10ac21edfc00c2bdb47ee987d99b4e559
Adwaith Jayan <148979632+Adwaith-Jayan@users.noreply.github.com>
2025-03-02 19:33:59 +0530

    Update primeClient.c

--- UDP/primeClient.c
+++ UDP/primeClient.c
@@ -4,40 +4,59 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-void main()
+int arr[20];
+
+int main()
 {
    int sockfd;
-   int port=8000;
-
+   int port = 8000;
    struct sockaddr_in server_addr;
-   sockfd=socket(AF_INET,SOCK_DGRAM,0);
-   bzero((char*)&server_addr,sizeof(server_addr));
-   server_addr.sin_family=AF_INET;
-   server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
-   server_addr.sin_port=htons(port);
+
+   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+   if (sockfd < 0)
+   {
+      perror("Socket creation failed");
+      exit(EXIT_FAILURE);
+   }
+
+   memset(&server_addr, 0, sizeof(server_addr));
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+   server_addr.sin_port = htons(port);
 
    char buffer[255];
-   int arr[20];
-   while(1)
-   {
-      bzero(buffer,255);
-      printf("Enter no: ");
-      fgets(buffer,255,stdin);
-      if(strncasecmp(buffer,"exit",4)==0)
-         break;
-      sendto(sockfd,buffer,255,0,(struct sockaddr*)&server_addr,sizeof(server_addr));
-      bzero(buffer,255);
-      bzero((char*)arr,20);
-      recvfrom(sockfd,buffer,255,0,NULL,NULL);
-      printf("Server: %s\n",buffer);
-      bzero((char*)&arr,20);
-      recvfrom(sockfd,arr,20,0,NULL,NULL);
-      int n=sizeof(arr)/sizeof(arr[0]);
-      printf("Server:\n");
-      printf("Factors are : ");
-      for(int i=0;i<n;i++)
-         printf("%d",arr[0]);
 
+   while (1)
+   {
+      memset(buffer, 0, sizeof(buffer));
+      printf("Enter number (or type 'exit' to quit): ");
+      fgets(buffer, sizeof(buffer), stdin);
+
+      buffer[strcspn(buffer, "\n")] = '\0';
+
+      if (strncasecmp(buffer, "exit", 4) == 0)
+         break;
+
+      sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+      memset(buffer, 0, sizeof(buffer));
+      recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL);
+      printf("Server: %s\n", buffer);
+
+      if (strcasecmp(buffer, "It is prime") != 0 && strcasecmp(buffer, "Not prime or composite") != 0)
+      {
+         memset(arr, 0, sizeof(arr));
+         recvfrom(sockfd, arr, sizeof(arr), 0, NULL, NULL);
+         printf("Factors: ");
+         for (int i = 0; i < 20; i++)
+         {
+            if (arr[i] != 0)
+               printf("%d ", arr[i]);
+         }
+         printf("\n");
+      }
    }
+
    close(sockfd);
+   return 0;
 }
